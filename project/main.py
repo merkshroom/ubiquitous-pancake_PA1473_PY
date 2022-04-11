e6b68bb6e8bed90619faea461962ddc1a4872648
Kalle Karlgren <kkarlgren@gmail.com>
2022-04-11 13:24:18 +0200

    Basic variables added

--- project/main.py
+++ project/main.py
@@ -2,6 +2,21 @@
 import sys
 import __init__
 
+import time
+from pybricks import robotics
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+
+Left_drive=Port.C 
+Right_drive=Port.B
+Crane_motor=Port.A
+Front_button=Port.S1
+Light_sensor=Port.S3
+Ultrasonic_sensor=Port.S4
+
 def main():
     return 0
 
